32add6ce43a88f5842e60a113099d96a455a0c0e
Laxmatt14 <matthew.fultz9@gmail.com>
2023-12-01 19:24:43 -0500

    Updating data functions for reviews

--- data/index.js
+++ data/index.js
@@ -1,7 +1,9 @@
 import * as accountDataFunctions from "./accounts.js";
 import * as userDataFunctions from "./users.js";
+import * as reviewDataFunctions from "./reviews.js";
 import barDataFunctions from "./bars.js";
 
 export const accountData = accountDataFunctions;
 export const userData = userDataFunctions;
 export const barData = barDataFunctions;
+export const reviewData = reviewDataFunctions;
