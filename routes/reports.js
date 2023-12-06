8e1bc909e25ae6a1b0f642e3586fbc5999d7be5b
gjonesdev <jonesgaetano@gmail.com>
2023-12-05 17:42:09 -0800

    cleaned up some account functionality

--- routes/reports.js
+++ routes/reports.js
@@ -3,53 +3,63 @@ import { Router } from "express";
 const router = Router();
 import * as validation from "../helpers.js";
 
-router.route("/").get(async (req, res) => {
-        res.render("reports", {
-                title: "Reports"
-        });
-    }).post(async (req, res) => {
+router
+	.route("/")
+	.get(async (req, res) => {
+		res.render("reports", {
+			title: "reports",
+		});
+	})
+	.post(async (req, res) => {
+		const blogReportData = req.body;
+		//make sure there is something present in the req.body
+		if (!blogReportData || Object.keys(blogReportData).length === 0) {
+			return res
+				.status(400)
+				.json({ error: "There are no fields in the request body" });
+		}
+		//check all inputs, that should respond with a 400
+		let userIdInput = "12345YesID";
+		let { /*userIdInput,*/ reasonInput, commentInput } = blogReportData;
+		try {
+			//UserId - Object Id:
+			//userId = validation.validateId(userId);
+			validation.validateReport(userIdInput, reasonInput, commentInput);
+			userIdInput = validation.validateUserId(userIdInput);
+			reasonInput = validation.validateReason(reasonInput);
+			commentInput = validation.validateComment(commentInput);
+		} catch (e) {
+			return res.status(400).render("error", {
+				class: "error",
+				message: e.toString(),
+				title: "error: 400",
+			});
+		}
+		//insert report 200
+		try {
+			//Create a new report and inserted in the database
+			const newReport = await reportsData.registerReport(
+				userIdInput,
+				reasonInput,
+				commentInput
+			);
 
-   const blogReportData = req.body;
-   //make sure there is something present in the req.body
-   if (!blogReportData || Object.keys(blogReportData).length === 0) {
-     return res
-       .status(400)
-       .json({error: 'There are no fields in the request body'});
-   }
-   //check all inputs, that should respond with a 400
-   let userIdInput = "12345YesID";
-   let {/*userIdInput,*/ reasonInput, commentInput} = blogReportData;
-   try {
-     //UserId - Object Id:
-     //userId = validation.validateId(userId);
-     validation.validateReport(userIdInput, reasonInput, commentInput);
-     userIdInput = validation.validateUserId(userIdInput);
-     reasonInput = validation.validateReason(reasonInput);
-     commentInput = validation.validateComment(commentInput);
-
-   } catch (e) {
-     return res.status(400).render("error", 
-     { 
-       class: "error", message: e.toString(), title: "Error: 400"
-     });
-   }
-   //insert report 200
-   try {
-    //Create a new report and inserted in the database
-    const newReport = await reportsData.registerReport(userIdInput, reasonInput, commentInput);
-    
-    if(newReport.insertedReport === true){
-      //res.redirect("login");
-    }
-    else{
-      res.status(500).render("error", {class: "error", message: "Internal server error", title: "Error: 500"});
-    }    
-   } catch (e) {
-     return res.status(500).render("error", 
-     { 
-        class: "error", message: "Internal server error", title: "Error: 500"
-     });
-   } 
-});
+			if (newReport.insertedReport === true) {
+				//res.redirect("login");
+			} else {
+				res.status(500).render("error", {
+					class: "error",
+					message: "Internal server error",
+					title: "Error: 500",
+				});
+			}
+		} catch (e) {
+			return res.status(500).render("error", {
+				class: "error",
+				message: "Internal server error",
+				title: "Error: 500",
+			});
+		}
+	});
 
 export default router;
