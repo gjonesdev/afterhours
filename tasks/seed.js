ff0ca0bab800260ed5c0f34b6c861c06ebe306a9
Laxmatt14 <matthew.fultz9@gmail.com>
2023-12-05 23:17:26 -0500

    updating seed for testing, commented out calls

--- tasks/seed.js
+++ tasks/seed.js
@@ -5,6 +5,48 @@ const db = await dbConnection();
 import bars from "../data/bars.js";
 import * as reviews from "../data/reviews.js"
 
+// try {
+//   let test = await bars.createBar(
+//     "GazBar",
+//     "The GazBar Sports Grill is a fresh, newly renovated sports bar and grill in Central Massachusetts. We offer everything your taste buds can handle and if you don’t see something you’d like….just ask and ourChef will personally create your meal. The GazBar offers a dining experience that Leominster has NEVER seen before.",
+//     {
+//       streetAddress: "1045 Central Street",
+//       city: "Leominster",
+//       state: "MA",
+//       zipCode: "01453",
+//     },
+//     "978-332-0595",
+//     "thegazbar@gazbar.com",
+//     "www.gazBar.com",
+//     "654438c26ec81bf9429dc36e",
+//     ["Sport", "Grill", "Margaritas"]
+//   );
+//   console.log(test);
+// } catch (e) {
+//   console.log(e);
+// }
+
+// try {
+//   let test = await bars.createBar(
+//     "Mellos's Mexican",
+//     "The GazBar Sports Grill is a fresh, newly renovated sports bar and grill in Central Massachusetts. We offer everything your taste buds can handle and if you don’t see something you’d like….just ask and ourChef will personally create your meal. The GazBar offers a dining experience that Leominster has NEVER seen before.",
+//     {
+//       streetAddress: "1045 Central Street",
+//       city: "Leominster",
+//       state: "MA",
+//       zipCode: "01453",
+//     },
+//     "789-568-4596",
+//     "thegazbar@gazbar.com",
+//     "www.gazBar.com",
+//     "654438c26ec81bf9429dc36e",
+//     ["Sport", "Grill", "Margaritas"]
+//   );
+//   console.log(test);
+// } catch (e) {
+//   console.log(e);
+// }
+
 // let test;
 // try {
 //   test = await bars.createBar(
@@ -16,16 +58,24 @@ import * as reviews from "../data/reviews.js"
 //       state: "MA",
 //       zipCode: "01453",
 //     },
-//     7194825444,
+//     "789-563-5555",
 //     "thegazbar@gazbar.com",
 //     "www.gazBar.com",
 //     "654438c26ec81bf9429dc36e",
-//     ["sprot", "Grill", "Margaritas"]
+//     ["Sport", "Grill", "Margaritas"]
 //   );
 //   console.log(test);
 // } catch (e) {
 //   console.log(e);
 // }
+
+/*
+try {
+  console.log(barss.barById("654696485139a59f569c79fd"));
+} catch (e) {
+  console.log(e);
+}
+*/
 
 // try {
 //   let review = await reviews.createReview(
@@ -53,10 +103,10 @@ import * as reviews from "../data/reviews.js"
 
 // try {
 //   let review = await reviews.updateReview(
-//     "656fd01245a80dfb2c134344"
+//     "656ff3ee8240eaf34c0facad"
 // ,   "accountId",
-//     "656fd01245a80dfb2c134342",
-//     5,
+//     "656ff3ee8240eaf34c0facac",
+//     1,
 //     "IT WORKED"
 //   );
 //   console.log(review)
@@ -66,7 +116,7 @@ import * as reviews from "../data/reviews.js"
 
 // try {
 //   let review = await reviews.get(
-//     "656fd01245a80dfb2c134344"
+//     "656ff3ee8240eaf34c0facad"
 //   );
 //   console.log(review)
 // } catch (e) {
@@ -75,7 +125,7 @@ import * as reviews from "../data/reviews.js"
 
 // try {
 //   let review = await reviews.getReviewsForBar(
-//     "656fcdc46fa718ba90252525"
+//     "656ff3ee8240eaf34c0facac"
 //   );
 //   console.log(review)
 // } catch (e) {
@@ -84,19 +134,12 @@ import * as reviews from "../data/reviews.js"
 
 // try {
 //   let review = await reviews.deleteReview(
-//     "656fcdc46fa718ba90252526",
-//     "656fcdc46fa718ba90252525"
+//     "656ff534e1b14d78299459f6",
+//     "656ff534e1b14d78299459f5"
 //   );
 //   console.log(review)
 // } catch (e) {
 //   console.log(e)
 // }
-/*
 
-try {
-  console.log(barss.barById("654696485139a59f569c79fd"));
-} catch (e) {
-  console.log(e);
-}
-*/
 await closeConnection();
