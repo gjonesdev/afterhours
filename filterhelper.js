5e6ce90d948bb232c2f32294dd9bcc35ec064709
mmedrano1985 <mmedrano@stevens.edu>
2023-12-10 21:36:45 -0500

    added events and bars sorted by dist

--- filterhelper.js
+++ filterhelper.js
@@ -4,13 +4,13 @@ import date from "date-and-time";
 
 //Global Variables
 let userCity = "";
+let barsDistanceList = [];
 
 let exportedMethods = {
   async barsDistance(userLocation, bars) {
     const myKey = "AIzaSyCNmw9imxqmAtqkfDn194OzvwuTwjMOZXw";
     const userLoc = userLocation.latitude + "," + userLocation.longitude;
 
-    let barsDistanceList = [];
     let destinations = "";
     bars.forEach((bar) => {
       if (bars[bars.length - 1] !== bar) {
@@ -43,20 +43,19 @@ let exportedMethods = {
 
     let userAddress = data.origin_addresses[0];
     userCity = userAddress.split(", ")[1];
-    barsDistanceList.push({ userCity: userCity });
 
     return barsDistanceList;
   },
-
+  // Finding the BOD
   async barOfTheDay(allBars) {
     //Variables
     const candidatesToBOD = [];
     const now = new Date();
     const today = date.format(now, "MM/DD/YYYY");
     const aBOD = [];
-    let hasEvent = true;
-    let cityBar = true;
-    let eventToday = true;
+    let hasEvent = false;
+    let cityBar = false;
+    let eventToday = false;
     const cityBars = [];
 
     //Cehcking if there is a BOD in the user's city
@@ -84,21 +83,17 @@ let exportedMethods = {
               if (event.date === today) {
                 eventToday = true;
                 candidatesToBOD.push(bar);
-              } else {
-                eventToday = false;
               }
             });
           }
-        } else {
-          cityBar = false;
         }
       });
       //Sorting BOD (bar of the date) candidates, first by rating and then by likes
-      const sortedCandidates = [];
+      let sortedCandidates = [];
       if (candidatesToBOD.length > 1) {
         sortedCandidates = candidatesToBOD.sort(function (a, b) {
           return (
-            a.ratingAverage - b.ratingAverage || b.likesCount - a.likesCount
+            b.ratingAverage - a.ratingAverage || a.likesCount - b.likesCount
           );
         });
         const BODId = sortedCandidates[0]._id.toString();
@@ -106,22 +101,56 @@ let exportedMethods = {
       } else if (candidatesToBOD.length === 1) {
         const BODId = candidatesToBOD[0]._id.toString();
         return barsFunctions.makeBOD(BODId);
-      } else if (!cityBar) {
+      } else if (!cityBars.length) {
         return {
           message: `We have not bars listed in ${userCity} yet. We are working on adding more  bars.`,
         };
       } else if (candidatesToBOD.length === 0 && !eventToday) {
-        sortedCandidates = cityBars.sort(function (a, b) {
-          return (
-            a.ratingAverage - b.ratingAverage || b.likesCount - a.likesCount
-          );
-        });
-        const BODId = sortedCandidates[0]._id.toString();
-        return barsFunctions.makeBOD(BODId);
+        if (cityBars.length > 1) {
+          sortedCandidates = cityBars.sort(function (a, b) {
+            return (
+              b.ratingAverage - a.ratingAverage || a.likesCount - b.likesCount
+            );
+          });
+          const BODId = sortedCandidates[0]._id.toString();
+          return barsFunctions.makeBOD(BODId);
+        } else {
+          const BODId = cityBars[0]._id.toString();
+          return barsFunctions.makeBOD(BODId);
+        }
       }
     } else {
       return aBOD[0];
     }
   },
+
+  async sortedBarsbyDistance() {
+    let barsAndDistance = [];
+
+    barsDistanceList.forEach((bar) => {
+      const { distance } = bar;
+      const barDist = distance.distance.text;
+      const barTime = distance.duration.text;
+      let tempArray = barDist.split(" ");
+      let numDistance = Number(tempArray[0]);
+      if (tempArray[1].toLowerCase() === "ft") {
+        numDistance = Math.round(numDistance * 0.0001894);
+      }
+      barsAndDistance.push({
+        bar: bar.bar,
+        distance: numDistance,
+        duration: barTime,
+      });
+    });
+
+    let sortedBars = barsAndDistance.sort(function (a, b) {
+      return (
+        a.distance - b.distance || b.bar.ratingAverage - a.bar.ratingAverage
+      );
+    });
+
+    return sortedBars;
+  },
 };
+
 export default exportedMethods;
