298dfacb722251b4deb989d99fc750bf392e78e2
gjonesdev <jonesgaetano@gmail.com>
2023-12-13 23:41:06 -0800

    chore: added styles for account pages feat: added filter button to bar page chore: updated css in various places

--- data/reviews.js
+++ data/reviews.js
@@ -4,278 +4,283 @@ import { bars } from "../config/mongoCollections.js";
 import * as validation from "../helpers.js";
 import barsFunctions from "./bars.js";
 
-export const createReview = async (accountId, firstName, barName, barId, rating, comment) => {
-  //accountId comes from the user leaving the review
-  //barId comes from the bar being reviewed
-  accountId = validation.validateRequiredStr(accountId);
-  firstName = validation.validateRequiredStr(firstName);
-  barName = validation.validateRequiredStr(barName);
-  barId = validation.validateRequiredStr(barId);
-  rating = validation.validateRequiredRating(rating);
-  comment = validation.validateOptionalStr(comment);
-  let date = new Date().toString();
-  const barCollection = await bars();
+export const createReview = async (
+	accountId,
+	firstName,
+	barName,
+	barId,
+	rating,
+	comment
+) => {
+	//accountId comes from the user leaving the review
+	//barId comes from the bar being reviewed
+	accountId = validation.validateRequiredStr(accountId);
+	firstName = validation.validateRequiredStr(firstName);
+	barName = validation.validateRequiredStr(barName);
+	barId = validation.validateRequiredStr(barId);
+	rating = validation.validateRequiredRating(rating);
+	comment = validation.validateOptionalStr(comment);
+	let date = new Date().toString();
+	const barCollection = await bars();
 
-  const theBar = await barsFunctions.barById(barId);
-  let totalRatings = rating;
-  theBar.reviews.forEach((review) => {
-    totalRatings += review.rating;
-  });
+	const theBar = await barsFunctions.barById(barId);
+	let totalRatings = rating;
+	theBar.reviews.forEach((review) => {
+		totalRatings += review.rating;
+	});
 
-  let ratingAverage = totalRatings / (theBar.reviews.length + 1);
+	let ratingAverage = totalRatings / (theBar.reviews.length + 1);
 
-  ratingAverage = parseFloat(ratingAverage.toFixed(2))
+	ratingAverage = parseFloat(ratingAverage.toFixed(2));
 
-  const review = {
-    accountId,
-    firstName,
-    barName,
-    barId,
-    rating,
-    comment,
-    date,
-  };
+	const review = {
+		accountId,
+		firstName,
+		barName,
+		barId,
+		rating,
+		comment,
+		date,
+	};
 
-  const reviewCollection = await reviews();
+	const reviewCollection = await reviews();
 
-  // Insert the review into the reviews collection
-  const res = await reviewCollection.insertOne(review);
+	// Insert the review into the reviews collection
+	const res = await reviewCollection.insertOne(review);
 
-  if (!res.acknowledged || !res.insertedId) {
-    throw "Failed to add review";
-  }
+	if (!res.acknowledged || !res.insertedId) {
+		throw "Failed to add review";
+	}
 
-  const newId = res.insertedId.toString();
-  const newReview = await get(newId);
+	const newId = res.insertedId.toString();
+	const newReview = await get(newId);
 
-  // Update the bar's reviews array
+	// Update the bar's reviews array
 
-  const updateBar = await barCollection.findOneAndUpdate(
-    { _id: new ObjectId(barId) },
-    {
-      $push: { reviews: newReview },
-      $set: { ratingAverage: ratingAverage },
-    },
-    { returnDocument: "after" }
-  );
+	const updateBar = await barCollection.findOneAndUpdate(
+		{ _id: new ObjectId(barId) },
+		{
+			$push: { reviews: newReview },
+			$set: { ratingAverage: ratingAverage },
+		},
+		{ returnDocument: "after" }
+	);
 
-  if (updateBar === null) {
-    throw "Bar update unsuccessful";
-  }
-  // call the bar again to update the rating and review count
-  const updatedBar = await barsFunctions.barById(barId);
-  const reviewsArray = updatedBar.reviews;
-  const addReviewCount = await barCollection.updateOne(
-    { _id: new ObjectId(barId) },
-    {
-      $push: { ratings: rating },
-      $set: { reviewsCount: reviewsArray.length },
-    }
-  );
-  if (addReviewCount.reviewsCount === 0) throw " Review could not be added!";
+	if (updateBar === null) {
+		throw "Bar update unsuccessful";
+	}
+	// call the bar again to update the rating and review count
+	const updatedBar = await barsFunctions.barById(barId);
+	const reviewsArray = updatedBar.reviews;
+	const addReviewCount = await barCollection.updateOne(
+		{ _id: new ObjectId(barId) },
+		{
+			$push: { ratings: rating },
+			$set: { reviewsCount: reviewsArray.length },
+		}
+	);
+	if (addReviewCount.reviewsCount === 0) throw " Review could not be added!";
 
-  return newReview;
+	return newReview;
 };
 
 export const getReviewsForBar = async (barId) => {
-  //Implement Code here
-  validation.validateId(barId);
-  const barCollection = await bars();
-  const bar = await barCollection.findOne({ _id: new ObjectId(barId) });
-  if (bar === null) {
-    throw `No bar with that id`;
-  }
+	//Implement Code here
+	validation.validateId(barId);
+	const barCollection = await bars();
+	const bar = await barCollection.findOne({ _id: new ObjectId(barId) });
+	if (bar === null) {
+		throw `No bar with that id`;
+	}
 
-  const reviews = bar.reviews;
+	const reviews = bar.reviews;
 
-  return reviews;
+	return reviews;
 };
 
 export const getReviewsByAccountId = async (accountId) => {
-  //Implement Code here
-  validation.validateId(accountId);
-  const reviewCollection = await reviews();
-  const reviewArray = await reviewCollection.find({ accountId: accountId }).toArray();
-  
-  return reviewArray;
+	//Implement Code here
+	validation.validateId(accountId);
+	const reviewCollection = await reviews();
+	const reviewArray = await reviewCollection
+		.find({ accountId: accountId })
+		.toArray();
+
+	return reviewArray;
 };
 
 export const deleteReview = async (reviewId, barId) => {
-  reviewId = validation.validateId(reviewId);
-  barId = validation.validateId(barId);
+	reviewId = validation.validateId(reviewId);
+	barId = validation.validateId(barId);
 
-  const reviewCollection = await reviews();
+	const reviewCollection = await reviews();
 
-  // Finding the review to delete
-  const reviewToDelete = await reviewCollection.findOne({
-    _id: new ObjectId(reviewId),
-  });
+	// Finding the review to delete
+	const reviewToDelete = await reviewCollection.findOne({
+		_id: new ObjectId(reviewId),
+	});
 
-  if (!reviewToDelete) throw "Could not find review";
+	if (!reviewToDelete) throw "Could not find review";
 
-  // Delete the review from the reviews collection
-  const deleteResult = await reviewCollection.deleteOne({
-    _id: new ObjectId(reviewId),
-  });
+	// Delete the review from the reviews collection
+	const deleteResult = await reviewCollection.deleteOne({
+		_id: new ObjectId(reviewId),
+	});
 
-  if (!deleteResult) {
-    throw "Review not found or already deleted";
-  }
+	if (!deleteResult) {
+		throw "Review not found or already deleted";
+	}
 
-  // Delete the review from the reviews array in the bar collection
-  const barCollection = await bars();
-  const bar = await barCollection.findOne({
-    _id: new ObjectId(barId),
-  });
+	// Delete the review from the reviews array in the bar collection
+	const barCollection = await bars();
+	const bar = await barCollection.findOne({
+		_id: new ObjectId(barId),
+	});
 
-  if (!bar) throw "Could not find bar";
+	if (!bar) throw "Could not find bar";
 
-  const removeReviewFromBar = await barCollection.findOneAndUpdate(
-    { _id: bar._id },
-    {
-      $pull: { reviews: { _id: reviewId } },
-    },
-    { returnDocument: "after" }
-  );
+	const removeReviewFromBar = await barCollection.findOneAndUpdate(
+		{ _id: bar._id },
+		{
+			$pull: { reviews: { _id: reviewId } },
+		},
+		{ returnDocument: "after" }
+	);
 
-  if (removeReviewFromBar === null) {
-    throw "Bar review update unsuccessful";
-  }
+	if (removeReviewFromBar === null) {
+		throw "Bar review update unsuccessful";
+	}
 
-  const theBar = await barsFunctions.barById(barId);
+	const theBar = await barsFunctions.barById(barId);
 
-  let totalRatings = 0;
-  let ratingAverage = 0;
-  if (theBar.reviews.length >= 1) {
-  
-    theBar.reviews.forEach((review) => {
-      totalRatings += review.rating;
-    });
+	let totalRatings = 0;
+	let ratingAverage = 0;
+	if (theBar.reviews.length >= 1) {
+		theBar.reviews.forEach((review) => {
+			totalRatings += review.rating;
+		});
 
-    ratingAverage = totalRatings / theBar.reviews.length;
+		ratingAverage = totalRatings / theBar.reviews.length;
 
-    ratingAverage = parseFloat(ratingAverage.toFixed(2))
-  }
+		ratingAverage = parseFloat(ratingAverage.toFixed(2));
+	}
 
-  const updateBarInfo = await barCollection.findOneAndUpdate(
-    { _id: bar._id },
-    {
-      $inc: { reviewsCount: -1 },
-      $set: { ratingAverage: ratingAverage },
-    },
-    { returnDocument: "after" }
-  );
+	const updateBarInfo = await barCollection.findOneAndUpdate(
+		{ _id: bar._id },
+		{
+			$inc: { reviewsCount: -1 },
+			$set: { ratingAverage: ratingAverage },
+		},
+		{ returnDocument: "after" }
+	);
 
-  return reviewToDelete;
+	return reviewToDelete;
 };
 
 export const updateReview = async (
-  reviewId,
-  firstName,
-  accountId,
-  barName,
-  barId,
-  rating,
-  comment
+	reviewId,
+	firstName,
+	accountId,
+	barName,
+	barId,
+	rating,
+	comment
 ) => {
-  reviewId = validation.validateId(reviewId);
-  accountId = validation.validateRequiredStr(accountId);
-  firstName = validation.validateRequiredStr(firstName);
-  barName = validation.validateRequiredStr(barName);
-  barId = validation.validateRequiredStr(barId);
-  rating = validation.validateRequiredRating(rating);
-  comment = validation.validateOptionalStr(comment);
-  let date = new Date().toString();
+	reviewId = validation.validateId(reviewId);
+	accountId = validation.validateRequiredStr(accountId);
+	firstName = validation.validateRequiredStr(firstName);
+	barName = validation.validateRequiredStr(barName);
+	barId = validation.validateRequiredStr(barId);
+	rating = validation.validateRequiredRating(rating);
+	comment = validation.validateOptionalStr(comment);
+	let date = new Date().toString();
 
-  const review = {
-    accountId,
-    firstName,
-    barName,
-    barId,
-    rating,
-    comment,
-    date,
-  };
+	const review = {
+		accountId,
+		firstName,
+		barName,
+		barId,
+		rating,
+		comment,
+		date,
+	};
 
-  const reviewCollection = await reviews();
+	const reviewCollection = await reviews();
 
-  //moved above
-  const reviewToDelete = await reviewCollection.findOne({
-    _id: new ObjectId(reviewId),
-  });
+	//moved above
+	const reviewToDelete = await reviewCollection.findOne({
+		_id: new ObjectId(reviewId),
+	});
 
-  if (!reviewToDelete) {
-    throw 'review not found'
-  }
+	if (!reviewToDelete) {
+		throw "review not found";
+	}
 
-  //here we are updating the review, the old review no longer will exist in the review DB
-  const updateInfo = await reviewCollection.findOneAndReplace(
-    { _id: new ObjectId(reviewId) },
-    review,
-    { returnDocument: "after" }
-  );
+	//here we are updating the review, the old review no longer will exist in the review DB
+	const updateInfo = await reviewCollection.findOneAndReplace(
+		{ _id: new ObjectId(reviewId) },
+		review,
+		{ returnDocument: "after" }
+	);
 
-  if (!updateInfo) {
-    throw "Update Unsuccessful";
-  }
+	if (!updateInfo) {
+		throw "Update Unsuccessful";
+	}
 
-  updateInfo._id = updateInfo._id.toString();
+	updateInfo._id = updateInfo._id.toString();
 
-  // Update the review in the reviews array in the bar collection
-  const barCollection = await bars();
-  const bar = await barCollection.findOne({
-    _id: new ObjectId(barId),
-  });
+	// Update the review in the reviews array in the bar collection
+	const barCollection = await bars();
+	const bar = await barCollection.findOne({
+		_id: new ObjectId(barId),
+	});
 
-  if (!bar) throw "Could not find bar";
+	if (!bar) throw "Could not find bar";
 
-  const updateBar = await barCollection.findOneAndUpdate(
-    { _id: bar._id,
-      "reviews._id" : reviewId
-    },
-        {$set: {"reviews.$": updateInfo}
-    },
-    {returnDocument:"after"}
-  );
+	const updateBar = await barCollection.findOneAndUpdate(
+		{ _id: bar._id, "reviews._id": reviewId },
+		{ $set: { "reviews.$": updateInfo } },
+		{ returnDocument: "after" }
+	);
 
-  if (updateBar === null) {
-    throw "Bar update unsuccessful";
-  }
+	if (updateBar === null) {
+		throw "Bar update unsuccessful";
+	}
 
-  const theBar = await barsFunctions.barById(barId);
-  
-  let totalRatings = 0;
-  theBar.reviews.forEach((review) => {
-    totalRatings += review.rating;
-  });
-  
-  let ratingAverage = totalRatings / (theBar.reviews.length);
+	const theBar = await barsFunctions.barById(barId);
 
-  ratingAverage = parseFloat(ratingAverage.toFixed(2))
+	let totalRatings = 0;
+	theBar.reviews.forEach((review) => {
+		totalRatings += review.rating;
+	});
 
-  const finalReviewArrayInBar = await barCollection.findOneAndUpdate(
-    { _id: bar._id },
-    {
-      $set: { ratingAverage: ratingAverage },
-    },
-    { returnDocument: "after" }
-  );
+	let ratingAverage = totalRatings / theBar.reviews.length;
 
-  if (finalReviewArrayInBar === null) {
-    throw "Bar update unsuccessful";
-  }
+	ratingAverage = parseFloat(ratingAverage.toFixed(2));
 
-  return updateInfo;
+	const finalReviewArrayInBar = await barCollection.findOneAndUpdate(
+		{ _id: bar._id },
+		{
+			$set: { ratingAverage: ratingAverage },
+		},
+		{ returnDocument: "after" }
+	);
+
+	if (finalReviewArrayInBar === null) {
+		throw "Bar update unsuccessful";
+	}
+
+	return updateInfo;
 };
 
 export const get = async (reviewId) => {
-  //Implement Code here
-  reviewId = validation.validateId(reviewId);
-  const eventCollection = await reviews();
-  const oneReview = await eventCollection.findOne({
-    _id: new ObjectId(reviewId),
-  });
-  if (oneReview === null) throw "No event with that id";
-  oneReview._id = oneReview._id.toString();
-  return oneReview;
+	//Implement Code here
+	reviewId = validation.validateId(reviewId);
+	const eventCollection = await reviews();
+	const oneReview = await eventCollection.findOne({
+		_id: new ObjectId(reviewId),
+	});
+	if (oneReview === null) throw "No event with that id";
+	oneReview._id = oneReview._id.toString();
+	return oneReview;
 };
--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
+import xss from "xss";
+
 export const rewriteUnsupportedBrowserMethods = (req, res, next) => {
 	// If the user posts to the server with a property called _method, rewrite the request's method
 	// To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
@@ -21,6 +23,11 @@ export const defaultRedirect = async (req, res, next, app) => {
 				: "Non-Authenticated User"
 		})`
 	);
+
+	req.body &&
+		Object.keys(req.body).forEach((input) => {
+			req.body[input] = xss(req.body[input]);
+		});
 
 	next();
 };
